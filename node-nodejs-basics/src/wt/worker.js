dcdae1826d2a461c900e45637119da30d40d7796
Eugene Kovgan <jugin.kovgan87@gmail.com>
2022-12-05 19:36:46 +0300

    feat: ready workers task

--- node-nodejs-basics/src/wt/worker.js
+++ node-nodejs-basics/src/wt/worker.js
@@ -1,7 +1,7 @@
 // n should be received from main thread
 import { workerData, parentPort } from 'worker_threads';
-const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
 
+export const nthFibonacci = (n) => (n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2));
 export const sendResult = () => {
   // This function sends result of nthFibonacci computations to main thread
   parentPort.postMessage(nthFibonacci(workerData));
