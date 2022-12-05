b0b60bb14e3d69ece2f75655e841a1d7aefb0845
Eugene Kovgan <jugin.kovgan87@gmail.com>
2022-12-05 14:47:24 +0300

    feat: add solution for zlib block

--- node-nodejs-basics/src/zip/decompress.js
+++ node-nodejs-basics/src/zip/decompress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from 'fs';
+import path from 'path';
+import { pipeline } from 'stream/promises';
+import * as zlib from 'zlib';
+import { getFullPath } from '../fs/common/getFullPath.js';
+
 const decompress = async () => {
-    // Write your code here 
+  const { __dirname } = getFullPath(import.meta.url);
+  const pathToFile = path.join(__dirname, 'files');
+
+  try {
+    const currentPath = path.join(pathToFile, 'archive.gz');
+    const newPath = path.join(pathToFile, 'fileToCompress.txt');
+    const unzip = zlib.createGunzip();
+
+    await pipeline(createReadStream(currentPath), unzip, createWriteStream(newPath));
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 await decompress();
